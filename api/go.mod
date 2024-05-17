19b3f34a1e251c5c5cd7c07d506bb589c76e1c4c
Rajan Walia <rajanwalia4@gmail.com>
2024-05-18 01:02:19 +0530

    Added url-shortner-code

--- api/go.mod
+++ api/go.mod
@@ -3,9 +3,11 @@ module github.com/rajanwalia4tech/url-shortner
 go 1.20
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.52.4
+	github.com/google/uuid v1.5.0
 	github.com/joho/godotenv v1.5.1
 )
 
@@ -14,7 +16,6 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
-	github.com/google/uuid v1.5.0 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- api/main.go
+++ api/main.go
@@ -5,16 +5,20 @@ import (
 	"log"
 	"os"
 
-	"./routes"
-
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
+	"github.com/rajanwalia4tech/url-shortner/routes"
 )
 
+// setup two routes, one for shortening the url
+// the other for resolving the url
+// for example if the short is `4fg`, the user
+// must navigate to `localhost:3000/4fg` to redirect to
+// original URL. The domain can be changes in .env file
 func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveURL)
-	app.Get("/:url", routes.shortenURL)
+	app.Post("/api/v1", routes.ShortenURL)
 }
 
 func main() {
@@ -22,10 +26,13 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	app := fiber.New()
 
+	//app.Use(csrf.New())
 	app.Use(logger.New())
+
 	setupRoutes(app)
-	log.Fatal(app.listen(os.Getenv("APP_PORT")))
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+	fmt.Println("Server has started on PORT ", os.Getenv("APP_PORT"))
 }
